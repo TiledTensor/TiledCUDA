ce78470df3316ec12af5988cb48ad95e75d751a7
KuangjuX <18630816527@163.com>
2024-04-18 22:33:33 -0700

    fix cute gemm tests.

--- tests/python/test_gemm.py
+++ tests/python/test_gemm.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     a = torch.randn(256, 256, device='cuda')
     b = torch.randn(256, 256, device='cuda')
     c = torch.empty(256, 256, device='cuda')
-    a_data = a.flatten()
-    b_data = b.flatten()
-    c_data = c.flatten()
-    torch.ops.tiledcuda.gemm(a, b, c, 256, 256, 256)
+    a_data = a.flatten().half()
+    b_data = b.flatten().half()
+    c_data = c.flatten().half()
 
-    ref_c = torch.mm(a, b)
+    torch.ops.tiledcuda.gemm(a_data, b_data, c_data, 256, 256, 256)
+    ref_c = torch.mm(a.half(), b.half())
 
-    print(c)
+    print(c_data)
     print(ref_c)
