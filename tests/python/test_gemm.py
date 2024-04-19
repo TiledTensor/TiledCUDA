71c92610e68552c5490ca205770f10581f3434fa
KuangjuX <18630816527@163.com>
2024-04-18 22:57:51 -0700

    Fix pytorch gemm unit test.

--- tests/python/test_gemm.py
+++ tests/python/test_gemm.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     c_data = c.flatten().half()
 
     torch.ops.tiledcuda.gemm(a_data, b_data, c_data, 256, 256, 256)
-    ref_c = torch.mm(a.half(), b.half())
+    ref_c = torch.mm(a.half(), b.half().t()).flatten()
 
     print(c_data)
     print(ref_c)
+
+    assert torch.allclose(c_data, ref_c, atol=1e-3)
