34e01ebc8e1928402dd927f2e0d2db45c11c4a95
KuangjuX <18630816527@163.com>
2024-04-19 04:06:49 -0700

    Add Back2Back GEMM unit test.

--- scripts/unittests/python.sh
+++ scripts/unittests/python.sh
@@ -1,1 +1,9 @@
-make unit_test UNIT_TEST=test_scatter_nd
+# for file in find(); do
+#     unit = $(basename $file .py)
+#     make unit_test UNIT_TEST=$unit
+# done
+
+for file in $(find tests/python -name *.py); do
+    unit=$(basename $file .py)
+    make unit_test UNIT_TEST=$unit
+done
