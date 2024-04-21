3b0e7dc707c543571e1119e2a0e8cbd65577a511
KuangjuX <18630816527@163.com>
2024-04-21 05:29:15 -0700

    Add Baatched GEMM kernel.

--- include/cell/traits/mod.hpp
+++ include/cell/traits/mod.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "cell/traits/b2b_gemm.hpp"
 #include "cell/traits/base.hpp"
+#include "cell/traits/batched_gemm.hpp"
 #include "cell/traits/gemm.hpp"
 #include "cell/traits/lstm.hpp"
--- include/kernels/mod.hpp
+++ include/kernels/mod.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "kernels/batched_gemm.hpp"
 #include "kernels/cute_b2b_gemm.hpp"
 #include "kernels/cute_gemm.hpp"
 #include "kernels/lstm_cell.hpp"
