1821c32bbf2c97fe33b3253ff8ac73b6c1f72acf
ChengXiang Qi <18630816527@163.com>
2024-04-21 09:57:45 +0800

    chore: Add Cuda Info functions. (#9)
    * chore: Add Cuda Info functions.
    
    * Add empty lines.

--- include/cuda_info.hpp
+++ include/cuda_info.hpp
@@ -1,8 +1,30 @@
 #pragma once
 
-namespace tiledcuda {
+#include "cuda_utils.hpp"
 
-// Returns the maximum number of threads per multiprocessor for the given GPU.
+#include <string>
+
+namespace tiledcuda {
+// Returns the number of GPUs.
+int GetGPUDeviceCount();
+
+// Returns the compute capability of the given GPU.
+int GetGPUComputeCapability(int id);
+
+// Returns the number of multiprocessors for the given GPU.
+int GetGPUMultiProcessors(int id);
+
+// Returns the maximum number of threads per multiprocessor for the given
+// GPU.
 int GetGPUMaxThreadsPerMultiProcessor(int id);
+
+// Returns the maximum number of threads per block for the given GPU.
+int GetGPUMaxThreadsPerBlock(int id);
+
+// Returns the maximum grid size for the given GPU.
+dim3 GetGpuMaxGridDimSize(int id);
+
+// Returns the name of the device.
+std::string GetDeviceName();
 
 }  // namespace tiledcuda
