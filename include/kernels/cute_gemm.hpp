14ac7fa8fc8dde199fa02f46e3fce4dc1223673e
ChengXiang Qi <18630816527@163.com>
2024-04-21 13:57:17 +0800

    chore: fix kernel function parameter type. (#10)
    * fix op.
    
    * Add empty lines.

--- include/kernels/cute_gemm.hpp
+++ include/kernels/cute_gemm.hpp
@@ -11,8 +11,8 @@ __global__ void dyn_cute_gemm_kernel(const Element* dA, const Element* dB,
 
 template <typename Element, typename InstructionShape, typename ValueMnk,
           typename WarpArrangement, typename CtaTileShape>
-void cute_gemm(const torch::Tensor& a, const torch::Tensor& b, torch::Tensor& c,
-               int m, int n, int k);
+void cute_gemm(const Element* a, const Element* b, Element* c, int m, int n,
+               int k);
 
 void custom_gemm_op(const torch::Tensor& a, const torch::Tensor& b,
                     torch::Tensor& c, int64_t m, int64_t n, int64_t k);
