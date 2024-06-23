7ea1fea9bf69232c8a770c21f5c66e993cbece1e
haruhi55 <haruhi55@126.com>
2024-06-22 23:24:59 -0700

    Upgrade the C++ standard to C++20.

--- scripts/cmake/generic.cmake
+++ scripts/cmake/generic.cmake
@@ -1,12 +1,12 @@
 set(CMAKE_BUILD_TYPE Release)
 
 set(CMAKE_CXX_STANDARD
-    17
+    20
     CACHE STRING "The C++ standard whoese features are requested." FORCE)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 set(CMAKE_CUDA_STANDARD
-    17
+    20
     CACHE STRING "The CUDA standard whose features are requested." FORCE)
 set(CMAKE_CUDA_STANDARD_REQUIRED ON)
 
@@ -21,12 +21,12 @@ set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS}  --Werror all-warnings")
 # to the device compiler
 set(CUDA_PROPAGATE_HOST_FLAGS OFF)
 
-# FIXME(haruhi): -std=c++17 has to be set explicitly here, Otherwise, linking
+# FIXME(haruhi): -std=c++20 has to be set explicitly here, Otherwise, linking
 # against torchlibs will raise errors. it seems that the host compilation
 # options are not passed to torchlibs.
-set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS} -std=c++17)
-set(CUDA_NVCC_FLAGS_DEBUG ${CUDA_NVCC_FLAGS_DEBUG} -std=c++17 -O0)
-set(CUDA_NVCC_FLAGS_RELEASE ${CUDA_NVCC_FLAGS_RELEASE} -std=c++17 -O3)
+set(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS} -std=c++20)
+set(CUDA_NVCC_FLAGS_DEBUG ${CUDA_NVCC_FLAGS_DEBUG} -std=c++20 -O0)
+set(CUDA_NVCC_FLAGS_RELEASE ${CUDA_NVCC_FLAGS_RELEASE} -std=c++20 -O3)
 
 find_package(PythonInterp 3 REQUIRED COMPONENTS Interpreter)
 message(STATUS "Python interpreter path: ${PYTHON_EXECUTABLE}")
