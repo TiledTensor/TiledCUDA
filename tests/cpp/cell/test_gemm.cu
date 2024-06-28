43ff147ae969b9bcb1f433921ead671b0ef66aa0
haruhi55 <haruhi55@126.com>
2024-06-28 03:12:09 -0700

    Refine the code organization of the GEMM unit test.

--- tests/cpp/cell/test_gemm.cu
+++ tests/cpp/cell/test_gemm.cu
@@ -8,6 +8,9 @@
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 
+#include <ctime>
+#include <random>
+
 namespace tiledcuda {
 
 using namespace cell;
@@ -18,8 +21,25 @@ using namespace cute;
 
 namespace {
 
-// This implementation interprets A and C as being laid out in row-major order,
-// while B is laid out in column-major order.
+float rand_float(float a = 1e-3, float b = 1) {
+    float random = ((float)rand()) / (float)RAND_MAX;
+    float diff = b - a;
+    float r = random * diff;
+    return a + r;
+}
+
+void check_correctness(const __half* hc1, const float* hc2, int numel) {
+    for (int i = 0; i < numel; ++i) {
+        printf("%.3f, ", __half2float(hc1[i]));
+        if (i && (i + 1) % 16 == 0) printf("\n");
+
+        // ASSERT_NEAR(hc1[i], hc2[i], 1e-3);
+    }
+    printf("\n");
+}
+
+//@brief: This implementation interprets A and C as being laid out in row-major
+//        order, while B is laid out in column-major order.
 void cublas_hgemm(int m, int n, int k, const __half* A, const __half* B,
                   __half* C, int lda, int ldb, int ldc) {
     __half alf = 1.;
@@ -32,14 +52,59 @@ void cublas_hgemm(int m, int n, int k, const __half* A, const __half* B,
     CublasCheck(cublasDestroy(handle));
 }
 
+template <typename Element, typename ElementAcc, typename WarpLayout,
+          const int M, const int N, const int K>
+struct TestTraits {
+    static const int kThreads = tl::get_numel<WarpLayout> * 32;
+
+    // for global to shared memory copy using CuTe
+    using LoadSharedA = traits::G2S2DCopyTraits<Element, M, K, M, K, kThreads,
+                                                false /*use swizzle*/>;
+    using LoadSharedB = traits::G2S2DCopyTraits<Element, N, K, N, K, kThreads,
+                                                false /*use swizzle*/>;
+
+    // transfer operand C from shared memory to global memory
+    using StoreSharedC =
+        traits::S2G2DCopyTraits<ElementAcc, M, N, M, N, kThreads,
+                                false /*use swizzle*/>;
+
+    using SharedA = SharedTile<Element, tl::RowMajor<M, K>>;
+    // [64, 128] is chunked by [32, 32], strip counts = [64/64, 128/32] = [1, 4]
+    using TileIteratorA = SharedTileIterator<SharedA, TileShape<64, 32>>;
+    using RegA = RegTile<Element, tl::RowMajor<4, 8>>;
+    using LoadRegA =
+        SharedToRegLoader<RegA, WarpLayout, WarpReuse::RowReuseCont,
+                          CopyInst::LoadMat>;
+
+    // A row-major Tile with a shape of [N, K] is equivalent to a column-major
+    // Tile with a shape of [K, N]
+    using SharedB = SharedTile<Element, tl::RowMajor<N, K>>;  // 64, 128
+    using RegB = RegTile<Element, tl::RowMajor<4, 8>>;
+    // [64, 128] is chunked by [32, 32], strip counts = [64/64, 128/32] = [1, 4]
+    using TileIteratorB = SharedTileIterator<SharedB, TileShape<64, 32>>;
+    using LoadRegB =
+        SharedToRegLoader<RegB, WarpLayout, WarpReuse::ColReuseCont,
+                          CopyInst::LoadMat>;
+
+    static_assert(TileIteratorA::sc1 == TileIteratorB::sc1,
+                  "dimension mismatch!");
+
+    using SharedC = SharedTile<ElementAcc, tl::RowMajor<M, N>>;  // 64, 64
+    using RegC = RegTile<ElementAcc, tl::RowMajor<4, 8>>;
+
+    using StoreRegC =
+        RegToSharedStorer<RegC, WarpLayout, RegLayout::WMMA_m16n16k16,
+                          CopyInst::LoadS32>;
+};
+
 template <typename Element, typename ElementAcc,                              //
           typename LoadSharedA, typename LoadSharedB, typename StoreSharedC,  //
           typename TileIteratorA, typename RegA, typename LoadRegA,
           typename TileIteratorB, typename RegB, typename LoadRegB,
           typename SharedC, typename RegC, typename StoreRegC>
-__global__ void test_wmma1(const Element* ga, const Element* gb, ElementAcc* gc,
-                           LoadRegA& load_rA, LoadRegB& load_rB,
-                           StoreRegC& store_rC) {
+__global__ void test_wmma(const Element* ga, const Element* gb, ElementAcc* gc,
+                          LoadRegA& load_rA, LoadRegB& load_rB,
+                          StoreRegC& store_rC) {
     extern __shared__ __align__(sizeof(double)) unsigned char buf_[];
     auto* shared_a = reinterpret_cast<Element*>(buf_);
     auto* shared_b = shared_a + TileIteratorA::Tile::kNumel;
@@ -83,30 +148,31 @@ __global__ void test_wmma1(const Element* ga, const Element* gb, ElementAcc* gc,
 
 namespace testing {
 
-TEST(TestWmma, shape1) {
-    // M, N, K for shared memory tile
-    const int M = 64;
-    const int N = 64;
-    const int K = 128;
-
-    // unittest for register-level gemm by calling into wmma PTX
+template <const int M, const int N, const int K, typename WarpLayout>
+void run_test_gemm() {
+    /// unittest for register-level gemm by calling into wmma PTX
     using Element = cutlass::half_t;
     using ElementAcc = float;
 
-    using WarpLayout = tl::RowMajor<2, 2>;
-    static const int kThreads = tl::get_numel<WarpLayout> * 32;
+    // initialize data
+    thrust::host_vector<Element> h_a(M * K);
+    thrust::host_vector<Element> h_b(K * N);
+    thrust::host_vector<ElementAcc> h_c(M * N);
+    for (int i = 0; i < h_a.size(); ++i)
+        // h_a[i] = static_cast<Element>(i % 2048 / 100.);
+        h_a[i] = static_cast<Element>(rand_float());
 
-    // for global to shared memory copy using CuTe
-    using LoadSharedA = traits::G2S2DCopyTraits<Element, M, K, M, K, kThreads,
-                                                false /*use swizzle*/>;
-    using LoadSharedB = traits::G2S2DCopyTraits<Element, N, K, N, K, kThreads,
-                                                false /*use swizzle*/>;
+    for (int i = 0; i < h_b.size(); ++i)
+        // h_b[i] = static_cast<Element>(i % 2048 / 100.);
+        h_b[i] = static_cast<Element>(rand_float());
+    thrust::fill(h_c.begin(), h_c.end(), 0.);
 
-    // transfer operand C from shared memory to global memory
-    using StoreSharedC =
-        traits::S2G2DCopyTraits<ElementAcc, M, N, M, N, kThreads,
-                                false /*use swizzle*/>;
+    thrust::device_vector<Element> d_a = h_a;
+    thrust::device_vector<Element> d_b = h_b;
+    thrust::device_vector<ElementAcc> d_c = h_c;
 
+    /// define the configuration of the test
+    using config = TestTraits<Element, ElementAcc, WarpLayout, M, N, K>;
     /*
      *  shared memory tile A [64, 128] is partitioned into a 2D grid of 64 x 32
      *  sub-tiles:
@@ -116,21 +182,12 @@ TEST(TestWmma, shape1) {
      *  |        |sub-tile 0|sub-tile 1|sub-tile 2|sub-tile 3|
      *  |--------|----------|----------|----------|----------|
      */
-    using SharedA = SharedTile<Element, tl::RowMajor<M, K>>;
-    // [64, 128] is chunked by [32, 32], strip counts = [64/64, 128/32] = [1, 4]
-    using TileIteratorA = SharedTileIterator<SharedA, TileShape<64, 32>>;
-    using RegA = RegTile<Element, tl::RowMajor<4, 8>>;
-    using LoadRegA =
-        SharedToRegLoader<RegA, WarpLayout, WarpReuse::RowReuseCont,
-                          CopyInst::LoadMat>;
-    LoadRegA load_rA;
-
-    LOG(INFO) << "TileIteratorA: [" << TileIteratorA::Tile::kRows << ", "
-              << TileIteratorA::Tile::kCols
-              << "]; numel = " << TileIteratorA::Tile::kNumel << std::endl
-              << "sc0 = " << TileIteratorA::sc0
-              << ", sc1 = " << TileIteratorA::sc1 << std::endl;
-
+    LOG(INFO) << "TileIteratorA: [" << config::TileIteratorA::Tile::kRows
+              << ", " << config::TileIteratorA::Tile::kCols
+              << "]; numel = " << config::TileIteratorA::Tile::kNumel
+              << std::endl
+              << "sc0 = " << config::TileIteratorA::sc0
+              << ", sc1 = " << config::TileIteratorA::sc1 << std::endl;
     /*
      *  shared memory tile B [128, 64] is partitioned into a 2D grid of 32 x 64
      *  sub-tiles:
@@ -146,93 +203,65 @@ TEST(TestWmma, shape1) {
      *  |   3    |sub-tile 6|
      *  |--------|----------|
      */
-
-    // A row-major Tile with a shape of [N, K] is equivalent to a column-major
-    // Tile with a shape of [K, N]
-    using SharedB = SharedTile<Element, tl::RowMajor<N, K>>;  // 64, 128
-    using RegB = RegTile<Element, tl::RowMajor<4, 8>>;
-    // [64, 128] is chunked by [32, 32], strip counts = [64/64, 128/32] = [1, 4]
-    using TileIteratorB = SharedTileIterator<SharedB, TileShape<64, 32>>;
-    using LoadRegB =
-        SharedToRegLoader<RegB, WarpLayout, WarpReuse::ColReuseCont,
-                          CopyInst::LoadMat>;
-    LoadRegB load_rB;
-
-    LOG(INFO) << "TileIteratorB: sc0 = " << TileIteratorB::sc0
-              << ", sc1 = " << TileIteratorB::sc1 << std::endl;
-
-    static_assert(TileIteratorA::sc1 == TileIteratorB::sc1,
-                  "dimension mismatch!");
-
-    using SharedC = SharedTile<ElementAcc, tl::RowMajor<M, N>>;  // 64, 64
-    using RegC = RegTile<ElementAcc, tl::RowMajor<4, 8>>;
-
-    using StoreRegC =
-        RegToSharedStorer<RegC, WarpLayout, RegLayout::WMMA_m16n16k16,
-                          CopyInst::LoadS32>;
-    StoreRegC store_rC;
-
+    LOG(INFO) << "TileIteratorB: sc0 = " << config::TileIteratorB::sc0
+              << ", sc1 = " << config::TileIteratorB::sc1 << std::endl
+              << "kThreads: " << config::kThreads << std::endl;
+    dim3 dim_grid(1, 1, 1);
+    dim3 dim_block(config::kThreads, 1, 1);
     int size_ab = (M + N) * K * sizeof(Element);
     int size_c = M * N * sizeof(ElementAcc);
     int shm_size = size_ab > size_c ? size_ab : size_c;
 
-    thrust::host_vector<Element> h_a(M * K);
-    thrust::host_vector<Element> h_b(K * N);
-    thrust::host_vector<ElementAcc> h_c(M * N);
+    typename config::LoadRegA load_rA;
+    typename config::LoadRegB load_rB;
+    typename config::StoreRegC store_rC;
 
-    for (int i = 0; i < h_a.size(); ++i)
-        h_a[i] = static_cast<Element>(i % 2048 / 100.);
-
-    for (int i = 0; i < h_b.size(); ++i)
-        h_b[i] = static_cast<Element>(i % 2048 / 100.);
-
-    thrust::fill(h_c.begin(), h_c.end(), 0.);
-
-    thrust::device_vector<Element> d_a = h_a;
-    thrust::device_vector<Element> d_b = h_b;
-    thrust::device_vector<ElementAcc> d_c = h_c;
-
-    LOG(INFO) << "kThreads: " << kThreads << std::endl;
-    dim3 dim_grid(1, 1, 1);
-    dim3 dim_block(kThreads, 1, 1);
-
-    test_wmma1<Element, ElementAcc, LoadSharedA, LoadSharedB, StoreSharedC,
-               TileIteratorA, RegA, LoadRegA, TileIteratorB, RegB, LoadRegB,
-               SharedC, RegC, StoreRegC><<<dim_grid, dim_block, shm_size>>>(
+    // TODO: Refine this code; there are too many template parameters, making it
+    // messy.
+    test_wmma<Element, ElementAcc, typename config::LoadSharedA,
+              typename config::LoadSharedB, typename config::StoreSharedC,
+              typename config::TileIteratorA, typename config::RegA,
+              typename config::LoadRegA, typename config::TileIteratorB,
+              typename config::RegB, typename config::LoadRegB,
+              typename config::SharedC, typename config::RegC,
+              typename config::StoreRegC><<<dim_grid, dim_block, shm_size>>>(
         thrust::raw_pointer_cast(d_a.data()),
         thrust::raw_pointer_cast(d_b.data()),
         thrust::raw_pointer_cast(d_c.data()), load_rA, load_rB, store_rC);
     cudaDeviceSynchronize();
+    h_c = d_c;
 
-    {
-        // unittest for correctness
-        // cublas as the ground-truth
-        thrust::device_vector<__half> d_cublas(M * N);
-        thrust::fill(d_cublas.begin(), d_cublas.end(), 0.);
+    /// unittest for correctness, take cublas as the ground-truth
+    thrust::device_vector<__half> d_cublas(M * N);
+    thrust::fill(d_cublas.begin(), d_cublas.end(), 0.);
 
-        // Matrix A has a row-major layout with dimensions [M, K],
-        // Matrix B has a column-major layout with dimensions [K, N],
-        // and Matrix C has a row-major layout with dimensions [M, N].
-        //
-        // This is equivalent to the following:
-        // Matrix A has a column-major layout with dimensions [K, M],
-        // Matrix B has a column-major layout with dimensions [K, N],
-        // and Matrix C has a column-major layout with dimensions [N, M].
-        // cuBlas is a Fortran-style(column-major) BLAS library,
-        // then we compute: C = B^T @ A
-        //             [N, M] = [N, K] @ [K, M]
-        cublas_hgemm(N, M, K,
-                     reinterpret_cast<const __half*>(
-                         thrust::raw_pointer_cast(d_b.data())),
-                     reinterpret_cast<const __half*>(
-                         thrust::raw_pointer_cast(d_a.data())),
-                     reinterpret_cast<__half*>(
-                         thrust::raw_pointer_cast(d_cublas.data())),
-                     K /*lda*/, K /*ldb*/, N /*ldc*/);
+    // Matrix A has a row-major layout with dimensions [M, K],
+    // Matrix B has a column-major layout with dimensions [K, N],
+    // and Matrix C has a row-major layout with dimensions [M, N].
+    //
+    // This is equivalent to the following:
+    // Matrix A has a column-major layout with dimensions [K, M],
+    // Matrix B has a column-major layout with dimensions [K, N],
+    // and Matrix C has a column-major layout with dimensions [N, M].
+    // cuBlas is a Fortran-style(column-major) BLAS library,
+    // then we compute: C = B^T @ A
+    //             [N, M] = [N, K] @ [K, M]
+    cublas_hgemm(
+        N, M, K,
+        reinterpret_cast<const __half*>(thrust::raw_pointer_cast(d_b.data())),
+        reinterpret_cast<const __half*>(thrust::raw_pointer_cast(d_a.data())),
+        reinterpret_cast<__half*>(thrust::raw_pointer_cast(d_cublas.data())),
+        K /*lda*/, K /*ldb*/, N /*ldc*/);
 
-        h_c = d_c;
-        thrust::host_vector<__half> h_groundtruth = d_cublas;
-    }
+    thrust::host_vector<__half> h_groundtruth = d_cublas;
+
+    check_correctness(thrust::raw_pointer_cast(h_groundtruth.data()),
+                      thrust::raw_pointer_cast(h_c.data()), M * N);
+}
+
+TEST(TestWmma, TestGemm) {
+    // M, N, K is this test is for shared memory tile
+    run_test_gemm<64, 64, 128, tl::RowMajor<2, 2>>();
 }
 
 }  // namespace testing
