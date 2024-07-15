eb5ad2ab2dc905a463bdc5c895382c2afdd19bad
KuangjuX <18630816527@163.com>
2024-07-15 03:51:03 +0000

    Add nested RegTile.

--- include/cell/copy/global_to_register.hpp
+++ include/cell/copy/global_to_register.hpp
@@ -5,9 +5,16 @@
 
 namespace tiledcuda::cell::copy {
 
-template <typename T, typename Layout, size_t height, size_t width>
-DEVICE void global_to_reg_load(T* src, RegTile<T, Layout>& dst,
-                               const int row_stride) {
+template <typename T, typename Layout>
+struct GlobalToRegLoader {
+    DEVICE void operator()(const T* global, RegTile<T, Layout>& dst);
+}
+
+template <typename T, size_t height, size_t width>
+DEVICE void load_global_to_reg(
+    T* src,
+    RegTile<RegTile<T, tl::RowMajor<2, 4>>, tl::RowMajor<height, width>>& dst,
+    const int row_stride) {
     // TODO: Fix constants.
     const int WARP_SIZE = 32;
     const int sub_tile_size = 16;
@@ -20,6 +27,10 @@ DEVICE void global_to_reg_load(T* src, RegTile<T, Layout>& dst,
     int sub_tile = sub_tile_size;
     int row_offset = rows * warp_id;
 
+    // RegTile<T, tl::RowMajor<2, 4>> subtile_reg;
+    // RegTile<RegTile<T, tl::RowMajor<2, 4>>, tl::RowMajor<height, width>>
+    //     dst_reg;
+
 // Load data from global memory to register.
 #pragma unroll
     for (int i = 0; i < height; ++i) {
@@ -27,18 +38,18 @@ DEVICE void global_to_reg_load(T* src, RegTile<T, Layout>& dst,
 #pragma unroll
         for (int j = 0; j < width; ++j) {
             int col = j * tile_size + (lane_id % 4);
-            dst(i, j + 0) = src[(row + 0) * row_stride + col + 0];
-            dst(i, j + 1) = src[(row + 0) * row_stride + col + 1];
-            dst(i, j + 4) = src[(row + 1) * row_stride + col + 8];
-            dst(i, j + 5) = src[(row + 1) * row_stride + col + 9];
+            dst(i, j)(0, 0) = src[(row + 0) * row_stride + col + 0];
+            dst(i, j)(0, 1) = src[(row + 0) * row_stride + col + 1];
+            dst(i, j)(1, 0) = src[(row + 0) * row_stride + col + 8];
+            dst(i, j)(1, 1) = src[(row + 0) * row_stride + col + 9];
         }
 #pragma unroll
         for (int j = 0; j < width; ++j) {
             int col = j * tile_size + (lane_id % 4);
-            dst(i, j + 2) = src[(row + 8) * row_stride + col + 0];
-            dst(i, j + 3) = src[(row + 8) * row_stride + col + 1];
-            dst(i, j + 6) = src[(row + 9) * row_stride + col + 8];
-            dst(i, j + 7) = src[(row + 9) * row_stride + col + 9];
+            dst(i, j)(0, 2) = src[(row + 8) * row_stride + col + 0];
+            dst(i, j)(0, 3) = src[(row + 8) * row_stride + col + 1];
+            dst(i, j)(1, 2) = src[(row + 8) * row_stride + col + 8];
+            dst(i, j)(1, 3) = src[(row + 8) * row_stride + col + 9];
         }
     }
 }
