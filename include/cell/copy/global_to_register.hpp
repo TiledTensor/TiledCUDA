ff0bad4e10748492db88b44c5b2f493eb7497b8c
KuangjuX <18630816527@163.com>
2024-07-15 13:31:31 +0000

    use functor instead of function.

--- include/cell/copy/global_to_register.hpp
+++ include/cell/copy/global_to_register.hpp
@@ -5,58 +5,58 @@
 
 namespace tiledcuda::cell::copy {
 
-/**
- * @brief Copy a 2D tile from global memory to register tile
- * @param src[in] source data in global memory.
- * @param dst[out] destination data in register tile.
- * @param row_stride[in] row stride of source data.
- */
-template <typename T, typename G2RTraits, size_t height, size_t width>
-DEVICE void copy_2d_tile_g2r(
-    const T* src,
-    RegTile<RegTile<T, tl::RowMajor<2, 4>>, tl::RowMajor<height, width>>& dst,
-    const int row_stride) {
-    static constexpr int WARP_SIZE = G2RTraits::WARP_SIZE;
-    const int sub_tile_size = G2RTraits::SubTileSize;
-    const int sub_rows = sub_tile_size;
-    int lane_id = threadIdx.x % WARP_SIZE;
-    int warp_id = threadIdx.x / WARP_SIZE;
-    int rows = height * sub_rows;
-    int tile_size = sub_tile_size;
-    int row_offset = rows * warp_id;
+// /**
+//  * @brief Copy a 2D tile from global memory to register tile
+//  * @param src[in] source data in global memory.
+//  * @param dst[out] destination data in register tile.
+//  * @param row_stride[in] row stride of source data.
+//  */
+// template <typename T, typename G2RTraits, size_t height, size_t width>
+// DEVICE void copy_2d_tile_g2r(
+//     const T* src,
+//     RegTile<RegTile<T, tl::RowMajor<2, 4>>, tl::RowMajor<height, width>>&
+//     dst, const int row_stride) { static constexpr int WARP_SIZE =
+//     G2RTraits::WARP_SIZE; const int sub_tile_size = G2RTraits::SubTileSize;
+//     const int sub_rows = sub_tile_size;
+//     int lane_id = threadIdx.x % WARP_SIZE;
+//     int warp_id = threadIdx.x / WARP_SIZE;
+//     int rows = height * sub_rows;
+//     int tile_size = sub_tile_size;
+//     int row_offset = rows * warp_id;
 
-    switch (G2RTraits::GLOBAL_LAYOUT) {
-        case tile_layout::GlobalLayout::RowMajor: {
-            // Load data from global memory to register.
-            // References:
-            // https://docs.nvidia.com/cuda/parallel-thread-execution/index.html?highlight=ldmatrix#warp-level-matrix-load-instruction-ldmatrix
-#pragma unroll
-            for (int i = 0; i < height; ++i) {
-                int row = row_offset + i * tile_size + (lane_id / 4);
-#pragma unroll
-                for (int j = 0; j < width; ++j) {
-                    int col = j * tile_size + (lane_id % 4);
-                    dst(i, j)(0, 0) = src[(row + 0) * row_stride + col + 0];
-                    dst(i, j)(0, 1) = src[(row + 0) * row_stride + col + 1];
-                    dst(i, j)(1, 0) = src[(row + 0) * row_stride + col + 8];
-                    dst(i, j)(1, 1) = src[(row + 0) * row_stride + col + 9];
-                }
-#pragma unroll
-                for (int j = 0; j < width; ++j) {
-                    int col = j * tile_size + (lane_id % 4);
-                    dst(i, j)(0, 2) = src[(row + 8) * row_stride + col + 0];
-                    dst(i, j)(0, 3) = src[(row + 8) * row_stride + col + 1];
-                    dst(i, j)(1, 2) = src[(row + 8) * row_stride + col + 8];
-                    dst(i, j)(1, 3) = src[(row + 8) * row_stride + col + 9];
-                }
-            }
-        }
-        case tile_layout::GlobalLayout::ColMajor: {
-            // TODO: Add support for ColMajor layout.
-            break;
-        }
-    }
-}
+//     switch (G2RTraits::GLOBAL_LAYOUT) {
+//         case tile_layout::GlobalLayout::RowMajor: {
+//             // Load data from global memory to register.
+//             // References:
+//             //
+//             https://docs.nvidia.com/cuda/parallel-thread-execution/index.html?highlight=ldmatrix#warp-level-matrix-load-instruction-ldmatrix
+// #pragma unroll
+//             for (int i = 0; i < height; ++i) {
+//                 int row = row_offset + i * tile_size + (lane_id / 4);
+// #pragma unroll
+//                 for (int j = 0; j < width; ++j) {
+//                     int col = j * tile_size + (lane_id % 4);
+//                     dst(i, j)(0, 0) = src[(row + 0) * row_stride + col + 0];
+//                     dst(i, j)(0, 1) = src[(row + 0) * row_stride + col + 1];
+//                     dst(i, j)(1, 0) = src[(row + 0) * row_stride + col + 8];
+//                     dst(i, j)(1, 1) = src[(row + 0) * row_stride + col + 9];
+//                 }
+// #pragma unroll
+//                 for (int j = 0; j < width; ++j) {
+//                     int col = j * tile_size + (lane_id % 4);
+//                     dst(i, j)(0, 2) = src[(row + 8) * row_stride + col + 0];
+//                     dst(i, j)(0, 3) = src[(row + 8) * row_stride + col + 1];
+//                     dst(i, j)(1, 2) = src[(row + 8) * row_stride + col + 8];
+//                     dst(i, j)(1, 3) = src[(row + 8) * row_stride + col + 9];
+//                 }
+//             }
+//         }
+//         case tile_layout::GlobalLayout::ColMajor: {
+//             // TODO: Add support for ColMajor layout.
+//             break;
+//         }
+//     }
+// }
 
 namespace details {
 
