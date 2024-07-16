d62f98f0299573bfcd04f7d343d97859af191dc2
KuangjuX <18630816527@163.com>
2024-07-16 07:42:57 +0000

    Add GlobalTile struct.

---
+++ include/types/global.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "types/layout.hpp"
+
+namespace tiledcuda::cell {
+
+using namespace cell;
+namespace tl = tile_layout;
+
+template <typename Element_, typename Layout_>
+struct GlobalTile {
+    using DType = Element_;
+    using Layout = Layout_;
+
+    static constexpr int kNumel = tl::get_numel<Layout>;
+
+    static constexpr int kRows = tl::num_rows<Layout>;
+    static constexpr int kCols = tl::num_cols<Layout>;
+
+    static constexpr int kRowStride = tl::row_stride<Layout>;
+    static constexpr int kColStride = tl::col_stride<Layout>;
+
+    static constexpr tl::Layout type = tl::layout_type<Layout>;
+
+    DEVICE GlobalTile(DType* data) : data_(data), layout_(Layout{}) {}
+
+    DEVICE DType* mutable_data() { return data_; }
+
+    DEVICE const DType* data() const { return data_; }
+
+    HOST_DEVICE const Layout& layout() const { return layout_; }
+
+    // for write access
+    DEVICE DType& operator()(int x, int y) { return data_[layout_(x, y)]; }
+
+    // for read access
+    DEVICE
+    const DType& operator()(int x, int y) const { return data_[layout_(x, y)]; }
+
+    DEVICE void dump_value() { print_tile(data_, layout_); }
+
+  private:
+    DType* data_;
+    Layout layout_;
+};
+}  // namespace tiledcuda::cell
