a84eb388d09f7071c234b5dbe04e01078608d386
KuangjuX <18630816527@163.com>
2024-07-16 07:26:05 +0000

    fix constanst names.

--- include/cell/copy/global_to_register.hpp
+++ include/cell/copy/global_to_register.hpp
@@ -20,25 +20,25 @@ struct LoadTileImpl {
 template <typename Element, typename Reg>
 struct LoadTileImpl<Element, Reg, tl::Layout::RowMajor> {
     using DType = Element;
-    static constexpr int Height = Reg::kRows;
-    static constexpr int Width = Reg::kCols;
-    static constexpr int SubTileSize = 16;
+    static constexpr int kHeight = Reg::kRows;
+    static constexpr int kWidth = Reg::kCols;
+    static constexpr int kSubTileSize = 16;
 
     DEVICE void operator()(const DType* src, Reg& dst, const int stride) {
         int lane_id = threadIdx.x % warpSize;
         int warp_id = threadIdx.x / warpSize;
-        const int tile_size = SubTileSize;
-        int rows = Height * SubTileSize;
+        const int tile_size = kSubTileSize;
+        int rows = kHeight * kSubTileSize;
         int row_offset = rows * warp_id;
 
         // Load data from global memory to register.
         // References:
         // https://docs.nvidia.com/cuda/parallel-thread-execution/index.html?highlight=ldmatrix#warp-level-matrix-load-instruction-ldmatrix
 #pragma unroll
-        for (int i = 0; i < Height; ++i) {
+        for (int i = 0; i < kHeight; ++i) {
             int row = row_offset + i * tile_size + (lane_id / 4);
 #pragma unroll
-            for (int j = 0; j < Width; ++j) {
+            for (int j = 0; j < kWidth; ++j) {
                 int col = j * tile_size + (lane_id % 4);
                 dst(i, j)(0, 0) = src[(row + 0) * stride + col + 0];
                 dst(i, j)(0, 1) = src[(row + 0) * stride + col + 1];
@@ -46,7 +46,7 @@ struct LoadTileImpl<Element, Reg, tl::Layout::RowMajor> {
                 dst(i, j)(1, 1) = src[(row + 0) * stride + col + 9];
             }
 #pragma unroll
-            for (int j = 0; j < Width; ++j) {
+            for (int j = 0; j < kWidth; ++j) {
                 int col = j * tile_size + (lane_id % 4);
                 dst(i, j)(0, 2) = src[(row + 8) * stride + col + 0];
                 dst(i, j)(0, 3) = src[(row + 8) * stride + col + 1];
