001a6a3c7ea17866fbc9c70a2b7eae97df935180
KuangjuX <18630816527@163.com>
2024-07-17 11:02:23 +0000

    Add comments.

--- include/types/base.hpp
+++ include/types/base.hpp
@@ -17,8 +17,10 @@ struct BaseTile {
     static constexpr int kRows = kTileSize;
     static constexpr int kCols = kTileSize;
     static constexpr int kNumel = kRows * kCols;
-    static constexpr int kNumelPerThread = kNumel / 32;  // 8
+    static constexpr int kNumelPerThread = kNumel / 32;           // 8
+    static constexpr int kPackedPerThread = kNumelPerThread / 2;  // 4
 
+    // 4 registers used in half/bf16, 8 registers used in float
     static constexpr int kRegsPerThread =
         sizeof(DType) * kNumelPerThread / sizeof(uint32);
 
