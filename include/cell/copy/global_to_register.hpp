86952a55b27ac339ac8456c60c02f17ba7519259
KuangjuX <18630816527@163.com>
2024-07-18 08:06:01 +0000

    refactor(g2r): Refactor Row-Major GlobalToReg Copy plan.

--- include/cell/copy/global_to_register.hpp
+++ include/cell/copy/global_to_register.hpp
@@ -1,11 +1,30 @@
 #pragma once
 
 #include "cell/copy/constants.hpp"
+#include "cell/copy/warp.hpp"
+#include "cell/traits/base.hpp"
 #include "types/mod.hpp"
 
 namespace tiledcuda::cell::copy {
 
+using namespace tiledcuda::cell::traits;
+
 namespace details {
+
+// template <typename Element, const tl::Layout type>
+// struct LoadBaseTile {
+//     using DType = Element;
+
+//     DEVICE void operator()(const DType* src, BaseTileRowMajor<DType>& dst) {
+//         int lane_id = threadIdx.x % warpSize;
+//     }
+// };
+
+// template <typename Element>
+// struct LoadBaseTile<Element, tl::Layout::RowMajor> {
+//     using DType = Element;
+//     using BaseTile = BaseTileRowMajor<Element>;
+// };
 
 template <typename Global, typename Reg, const tl::Layout type>
 struct GlobalToRegLoaderImpl {
@@ -78,6 +97,103 @@ struct GlobalToRegLoader {
     DEVICE void operator()(const Global& src, Reg& dst) {
         details::GlobalToRegLoaderImpl<Global, Reg, type_> loader;
         loader(src, dst);
+    }
+};
+
+template <typename Global_, typename Reg_, const int kRowExec_,
+          const int kColExec_, const tl::Layout type>
+struct GlobalToRegWarpLoaderImpl {
+    using Global = Global_;
+    using Reg = Reg_;
+    using DType = Global::type;
+
+    DEVICE void operator()(const DType* src, Reg& dst);
+};
+
+template <typename Global_, typename Reg_, const int kRowExec_,
+          const int kColExec_>
+struct GlobalToRegWarpLoaderImpl<Global_, Reg_, kRowExec_, kColExec_,
+                                 tl::Layout::RowMajor> {
+    using Global = Global_;
+    using Reg = Reg_;
+    using DType = Global::DType;
+
+    // strides to iterate over each 16x128-bits `BaseTile`.
+    static constexpr int kTileRstride =
+        BaseTileShape<DType>::row * Global::kRowStride;
+    static constexpr int kTileCstride = BaseTileShape<DType>::col;
+
+    // row stride and col stride for a thread in a warp
+    static constexpr int kStride = traits::TraitsBase<DType>::kNumPerAccess;
+    static constexpr int kLaneRstride = Global::kRowStride;
+    static constexpr int kLaneCstride = kStride;
+
+    static constexpr int kRowExec = kRowExec_;
+    static constexpr int kColExec = kColExec_;
+
+    DEVICE void operator()(const DType* src, Reg& dst) {
+        int lane_id = threadIdx.x % warpSize;
+
+        const DType* data = src;
+#pragma unroll
+        for (int i = 0; i < kRowExec; ++i) {
+            // TODO: magic number.
+            int row = i * 16 + lane_id / 4;
+#pragma unroll
+            for (int j = 0; j < kColExec; ++j) {
+                // TODO: magic number.
+                int col = j * 16 + lane_id % 4;
+                dst(i, j)(0, 0) =
+                    data[(row + 0) * Global::kRowStride + col + 0];
+                dst(i, j)(0, 1) =
+                    data[(row + 0) * Global::kRowStride + col + 1];
+                dst(i, j)(1, 0) =
+                    data[(row + 0) * Global::kRowStride + col + 8];
+                dst(i, j)(1, 1) =
+                    data[(row + 0) * Global::kRowStride + col + 9];
+                dst(i, j)(0, 2) =
+                    data[(row + 8) * Global::kRowStride + col + 0];
+                dst(i, j)(0, 3) =
+                    data[(row + 8) * Global::kRowStride + col + 1];
+                dst(i, j)(1, 2) =
+                    data[(row + 8) * Global::kRowStride + col + 8];
+                dst(i, j)(1, 3) =
+                    data[(row + 8) * Global::kRowStride + col + 9];
+            }
+        }
+    }
+};
+
+template <typename Global_, typename Reg_, typename WarpLayout_,
+          const WarpReuse kMode_,
+          typename Base = warp::CopyBase<WarpLayout_, kMode_>>
+struct GlobalToRegWarpLoader : public Base {
+    using Global = Global_;
+    using Reg = Reg_;
+    using DType = typename Global::DType;
+
+    using WarpLayout = WarpLayout_;
+    static constexpr WarpReuse kMode = kMode_;
+
+    DEVICE void operator()(const Global& src, Reg& dst) {
+        const DType* src_ptr = src.data();
+
+        // 1. advance the pointer to input data to the current warp according to
+        // warp reuse mode.
+        src_ptr += Base::template get_warp_offset<Global>();
+
+        // how many times a `BaseTile` is executed along the row and column
+        // direction.
+        static constexpr int kRowExec =
+            Base::template row_exec_count<DType, Global::kRows>();
+        static constexpr int kColExec =
+            Base::template col_exec_count<DType, Global::kCols>();
+
+        // TODO: Load data from global memory to register.
+        using Loader = GlobalToRegWarpLoaderImpl<Global, Reg, kRowExec,
+                                                 kColExec, Global::type>;
+        Loader loader;
+        loader(src_ptr, dst);
     }
 };
 
--- tests/cpp/cell/test_g2r_copy.cu
+++ tests/cpp/cell/test_g2r_copy.cu
@@ -1,3 +1,4 @@
+#include "cell/copy/constants.hpp"
 #include "cell/copy/mod.hpp"
 #include "common/test_utils.hpp"
 #include "types/mod.hpp"
@@ -9,17 +10,18 @@ namespace tiledcuda {
 
 using namespace cell;
 
-template <typename Element, tl::Layout type, size_t height, size_t width>
+template <typename Element, tl::Layout type, typename WarpLayout,
+          const copy::WarpReuse kMode, size_t height, size_t width>
 __global__ void copy_g2r_row_major(Element* src) {
     using SrcTile = GlobalTile<Element, tl::RowMajor<16 * height, 16 * width>>;
-    // using DstTile = RegTile<BaseFloatTileRowMajor, tl::RowMajor<height,
-    // width>>;
     using DstTile =
         RegTile<BaseTileRowMajor<Element>, tl::RowMajor<height, width>>;
     SrcTile src_tile(src);
     DstTile dst_tile;
 
-    cell::copy::GlobalToRegLoader<SrcTile, DstTile, type> loader;
+    cell::copy::GlobalToRegWarpLoader<SrcTile, DstTile, WarpLayout, kMode>
+        loader;
+    // cell::copy::GlobalToRegLoader<SrcTile, DstTile, type> loader;
     loader(src_tile, dst_tile);
     __syncthreads();
 
@@ -35,6 +37,7 @@ __global__ void copy_g2r_row_major(Element* src) {
 namespace testing {
 TEST(TestG2RegCopy, copy_2d_tile_g2r_row_major_0) {
     using Element = float;
+    using WarpLayout = tl::RowMajor<1, 1>;
 
     const int height = 1;
     const int width = 1;
@@ -47,27 +50,29 @@ TEST(TestG2RegCopy, copy_2d_tile_g2r_row_major_0) {
 
     thrust::device_vector<Element> d_src = h_src;
 
-    copy_g2r_row_major<Element, tl::Layout::RowMajor, height, width>
+    copy_g2r_row_major<Element, tl::Layout::RowMajor, WarpLayout,
+                       copy::WarpReuse::RowReuseCont, height, width>
         <<<1, 32>>>(d_src.data().get());
 }
 
-TEST(TestG2RegCopy, copy_2d_tile_g2r_row_major_1) {
-    using Element = float;
+// TEST(TestG2RegCopy, copy_2d_tile_g2r_row_major_1) {
+//     using Element = float;
 
-    const int height = 2;
-    const int width = 2;
+//     const int height = 2;
+//     const int width = 2;
 
-    int numel = height * width * 16 * 16;
-    thrust::host_vector<Element> h_src(numel);
-    for (int i = 0; i < numel; ++i) {
-        h_src[i] = (float)i;
-    }
+//     int numel = height * width * 16 * 16;
+//     thrust::host_vector<Element> h_src(numel);
+//     for (int i = 0; i < numel; ++i) {
+//         h_src[i] = (float)i;
+//     }
 
-    thrust::device_vector<Element> d_src = h_src;
+//     thrust::device_vector<Element> d_src = h_src;
 
-    copy_g2r_row_major<Element, tl::Layout::RowMajor, height, width>
-        <<<1, 32>>>(d_src.data().get());
-}
+//     copy_g2r_row_major<Element, tl::Layout::RowMajor, height, width>
+//         <<<1, 32>>>(d_src.data().get());
+// }
+
 }  // namespace testing
 
 }  // namespace tiledcuda
