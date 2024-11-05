a36c2d92d0de972ddf8739d780e4945f200bb13b
KuangjuX <18630816527@163.com>
2024-11-05 05:44:52 +0000

    Clean up leagcy traits code.

--- include/cell/traits/mod.hpp
+++ include/cell/traits/mod.hpp
@@ -1,8 +1,4 @@
 #pragma once
 
-#include "cell/traits/b2b_gemm.hpp"
 #include "cell/traits/base.hpp"
-#include "cell/traits/bmm.hpp"
 #include "cell/traits/copy.hpp"
-#include "cell/traits/gemm.hpp"
-#include "cell/traits/lstm.hpp"
