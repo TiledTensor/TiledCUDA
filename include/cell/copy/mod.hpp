7845b913618869dbdb0e7aa5a378467040fcc174
KuangjuX <18630816527@163.com>
2024-11-05 08:49:43 +0000

    Add comments.

--- include/cell/copy/mod.hpp
+++ include/cell/copy/mod.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "cell/copy/constants.hpp"
+// TODO(KuangjuX): Clean up `cell/copy/copy.hpp` will cause serious compilation errors, 
+// the reason is currently unclear.
 #include "cell/copy/copy.hpp"
 #include "cell/copy/copy_atom.hpp"
 #include "cell/copy/global_to_register.hpp"
